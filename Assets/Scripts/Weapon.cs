9cd4c05037bb206a79c1b2914d9faad48ad46531
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-02-19 15:36:01 +0200

    Shoot Coldown

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,40 +1,16 @@
-using StarterAssets;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    [SerializeField] WeaponSO weaponSO;
-    [SerializeField] GameObject hitVFXPrefab;
-    [SerializeField] Animator animator;
     [SerializeField] ParticleSystem muzzleFlash;
-    StarterAssetsInputs starterAssetsInputs;
-
-    const string SHOOT_STRING = "Shoot";
-
-    private void Awake()
-    {
-        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
-    }
-
-    void Update()
-    {
-        HandleShoot();
-    }
-
-    void HandleShoot()
-    {
-        if (!starterAssetsInputs.shoot) return;
-
-        muzzleFlash.Play();
-        animator.Play(SHOOT_STRING, 0, 0f);
-        starterAssetsInputs.ShootInput(false);
-
+    public void Shoot(WeaponSO weaponSO)
+    {   
         RaycastHit hit;
+        muzzleFlash.Play();
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
-        {
-            Instantiate(hitVFXPrefab, hit.point, quaternion.identity);
+        {            
+            Instantiate(weaponSO.HitVFXPrefab, hit.point, Quaternion.identity);
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(weaponSO.Damage);
         }
