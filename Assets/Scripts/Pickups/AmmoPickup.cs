ea213b4eaf2e1ba322846cebe63e10a61e7e1f73
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-02-20 22:33:01 +0200

    Ammo Pickup

--- Assets/Scripts/Pickups/AmmoPickup.cs
+++ Assets/Scripts/Pickups/AmmoPickup.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 
-public class AmmoPickup : MonoBehaviour
+public class AmmoPickup : Pickup
 {
-   
+    [SerializeField] int ammoAmount = 100;
+    protected override void OnPickup(ActiveWeapon activeWeapon)
+    {
+        activeWeapon.AdjustAmmo(ammoAmount);
+    }
 }
