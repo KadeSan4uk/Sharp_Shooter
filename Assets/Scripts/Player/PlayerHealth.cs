5a40407bda071d695cf90b9edfc4c01ac0f049fe
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-02-24 14:37:32 +0200

    Player Shield UI

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,11 +1,15 @@
 using Cinemachine;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [Range(1, 10)]
     [SerializeField] int health = 5;
     [SerializeField] CinemachineVirtualCamera deathVirtualCamera;
     [SerializeField] Transform weaponCamera;
+    [SerializeField] Image[] shieldBars;
 
     int currentHealth;
     int gameOverVirtualCameraPriority = 20;
@@ -13,17 +17,35 @@ public class PlayerHealth : MonoBehaviour
     private void Awake()
     {
         currentHealth = health;
+        AdjustShieldUI();
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
 
+        AdjustShieldUI();
+
         if (currentHealth <= 0)
         {
             weaponCamera.parent = null;
             deathVirtualCamera.Priority = gameOverVirtualCameraPriority;
             Destroy(this.gameObject);
+        }
+    }
+
+    private void AdjustShieldUI()
+    {
+        for (int i = 0; i < shieldBars.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                shieldBars[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                shieldBars[i].gameObject.SetActive(false);
+            }
         }
     }
 }
